0586c2611cde13aed06d68587ddf30106250f05c
Jonathan Monir <jonmonir@yahoo.com>
2023-03-07 17:15:23 +0200

    z

--- gadwal-getter.py
+++ gadwal-getter.py
@@ -41,8 +41,10 @@ def linear_alg_sec(rakam_el_gloos):
         return False
     return rkm_el_section
 
-path = "sections"
-files = os.listdir(path)
+sections_path = "sections"
+
+path_file = str(sections_path +"\\")
+files = os.listdir(sections_path)
 import streamlit as st
 
 name = st.text_input("Enter your name:")
@@ -52,65 +54,66 @@ if st.button("Submit"):
     else:
         st.success(f"Hello {name}!")
         for file in files:
-            count_got=0
-            pdf_file = open(path+"\\"+file, 'rb')    
+            if i.endswith(".pdf"):
+                count_got=0
+                pdf_file = open(path_file+file, 'rb')    
 
-            # creating a pdf reader object
-            pdf_reader = PyPDF2.PdfReader(pdf_file)
-
-
-            # extracting text from page
+                # creating a pdf reader object
+                pdf_reader = PyPDF2.PdfReader(pdf_file)
 
 
-            count_names=0
-            for el_saf7a in pdf_reader.pages:
-                text = el_saf7a.extract_text()
-                text = text.split('\n')
-                gadwal_el_sacation = list()
-                for i in text:
-                    if name in i:
-                        if count_names == 0:
-                            print(i)
-                        count_names += 1
-                        
-                        rkm_gloos = i.split(" ")[0]
-                        rkm_gloos = int(rkm_gloos)
-                if count_names == 1:
-                    text = pdf_reader.pages[0].extract_text().split('\n')
+                # extracting text from page
+
+
+                count_names=0
+                for el_saf7a in pdf_reader.pages:
+                    text = el_saf7a.extract_text()
+                    text = text.split('\n')
+                    gadwal_el_sacation = list()
                     for i in text:
-                        if "Sec " in i:
-                            gadwal_el_sacation.append(i)
+                        if name in i:
+                            if count_names == 0:
+                                print(i)
+                            count_names += 1
                             
-                    gadwal_el_sacation = ''.join(gadwal_el_sacation)
-                    each_Sec = gadwal_el_sacation.split("Sec")
+                            rkm_gloos = i.split(" ")[0]
+                            rkm_gloos = int(rkm_gloos)
+                    if count_names == 1:
+                        text = pdf_reader.pages[0].extract_text().split('\n')
+                        for i in text:
+                            if "Sec " in i:
+                                gadwal_el_sacation.append(i)
+                                
+                        gadwal_el_sacation = ''.join(gadwal_el_sacation)
+                        each_Sec = gadwal_el_sacation.split("Sec")
 
-                    for i in each_Sec[1:]:
-                        range_arkam_elGloos = i.split(" ")[3:5]
-                        range_arkam_elGloos = [int(i) for i in range_arkam_elGloos]
-                        
-                        if file == "OR.pdf" and between(rkm_gloos,1,192):
-                            rkm_el_section=or_sec(rkm_gloos)
-                            if count_got==0:
-                                section_text = f"section: {rkm_el_section} in {file.strip('.')}"
-                                st.write(section_text)
-                            count_got +=1
-                        elif file == "Linear Algebra.pdf" and between(rkm_gloos,1,186):
-                            rkm_el_section=linear_alg_sec(rkm_gloos)
-                            if count_got==0:
-                                section_text = f"section: {rkm_el_section} in {file.strip('.')}"
-                                st.write(section_text)
-                            count_got +=1
-                        elif rkm_gloos >= range_arkam_elGloos[0] and rkm_gloos <= range_arkam_elGloos[1]:
-                            if count_got==0:
-                                rkm_el_section = i.split(" ")[1]
-                                section_text = f"section: {rkm_el_section} in {file.strip('.')}"
-                                st.write(section_text)
-                            count_got +=1
+                        for i in each_Sec[1:]:
+                            range_arkam_elGloos = i.split(" ")[3:5]
+                            range_arkam_elGloos = [int(i) for i in range_arkam_elGloos]
+                            
+                            if file == "OR.pdf" and between(rkm_gloos,1,192):
+                                rkm_el_section=or_sec(rkm_gloos)
+                                if count_got==0:
+                                    section_text = f"section: {rkm_el_section} in {file.strip('.')}"
+                                    st.write(section_text)
+                                count_got +=1
+                            elif file == "Linear Algebra.pdf" and between(rkm_gloos,1,186):
+                                rkm_el_section=linear_alg_sec(rkm_gloos)
+                                if count_got==0:
+                                    section_text = f"section: {rkm_el_section} in {file.strip('.')}"
+                                    st.write(section_text)
+                                count_got +=1
+                            elif rkm_gloos >= range_arkam_elGloos[0] and rkm_gloos <= range_arkam_elGloos[1]:
+                                if count_got==0:
+                                    rkm_el_section = i.split(" ")[1]
+                                    section_text = f"section: {rkm_el_section} in {file.strip('.')}"
+                                    st.write(section_text)
+                                count_got +=1
 
 
-                    # closing the pdf file object
-            if count_names >1: 
-                st.error("more than one name specified in",file)
-            elif count_names==0:
-                st.error("no names specified in",file)
+                        # closing the pdf file object
+                if count_names >1: 
+                    st.error("more than one name specified in",file)
+                elif count_names==0:
+                    st.error("no names specified in",file)
         pdf_file.close()
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+import os
 path = "sections"
 sections_path = "sections"
 
 path_file = str(sections_path +"\\")
-print(path_file)
+files = os.listdir(sections_path)
+for i in files:
+    if i.endswith(".pdf"):
+        print(i)
